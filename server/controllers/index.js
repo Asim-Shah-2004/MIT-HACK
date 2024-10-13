7ab6139a35f0a344e57bf78a467ea520ea5eb96c
asim <asimshah2111@gmail.com>
2024-10-13 17:48:57 +0530

    added register routes

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,10 +2,14 @@ import registerSME from "./userControllers/registerSME.js";
 import addPost from "./communityControllers/addPost.js";
 import addComment from "./communityControllers/commentPost.js";
 import addLike from "./communityControllers/LikePost.js";
+import registerInvestor from "./userControllers/registerInvestor.js"
+import registerMentor from "./userControllers/registerMentor.js";
 
 export{
     registerSME,
     addPost,
     addComment,
-    addLike
+    addLike,
+    registerInvestor,
+    registerMentor
 }

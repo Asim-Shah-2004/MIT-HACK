ce02e3d61e52ad1618958fb2ca253035ae183733
asim <asimshah2111@gmail.com>
2024-10-15 18:05:51 +0530

    added server 2

---
+++ server2/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import "dotenv/config";
+
+import {logger} from './utils/index.js';
+import { connectDB } from "./services/index.js";
+
+const PORT = process.env.PORT;
+const app = express();
+
+connectDB();
+
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World</h1>');
+});
+
+
+app.listen(PORT, () => {
+    logger.info(`Server is running on port ${PORT}`);
+});
